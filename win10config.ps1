e2161caacf0d2dbee4716180e9d1ba71cbc24b36
brad <bradleynielsen@gmail.com>
2022-11-13 18:19:56 -0600

    startmenu

--- win10config.ps1
+++ win10config.ps1
@@ -10,6 +10,9 @@ $regPathPolicies         = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Poli
 $regPathPoliciesExplorer = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer"
 $regPathSearch           = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Search"
 
+
+$regPathDefaultAccount   = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\CloudStore\Store\Cache\DefaultAccount"
+
 #region Taskbar 
 
     #show all in tray
@@ -66,7 +69,6 @@ $regPathSearch           = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Sear
     Set-ItemProperty -Path $regPathSearch -Name "SearchboxTaskbarMode" -Value "00000000"
 
 
-    Get-Process -Name explorer | Stop-Process
 
 #endregion
 
@@ -91,7 +93,61 @@ $regPathSearch           = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Sear
 
 
 
+#region clear start menu
 
+$START_MENU_LAYOUT = @"
+<LayoutModificationTemplate xmlns:defaultlayout="http://schemas.microsoft.com/Start/2014/FullDefaultLayout" xmlns:start="http://schemas.microsoft.com/Start/2014/StartLayout" Version="1" xmlns:taskbar="http://schemas.microsoft.com/Start/2014/TaskbarLayout" xmlns="http://schemas.microsoft.com/Start/2014/LayoutModification">
+    <LayoutOptions StartTileGroupCellWidth="6" />
+    <DefaultLayoutOverride>
+        <StartLayoutCollection>
+            <defaultlayout:StartLayout GroupCellWidth="6" />
+        </StartLayoutCollection>
+    </DefaultLayoutOverride>
+</LayoutModificationTemplate>
+"@
+
+$layoutFile="C:\Windows\StartMenuLayout.xml"
+
+#Delete layout file if it already exists
+If(Test-Path $layoutFile)
+{
+    Remove-Item $layoutFile
+}
+
+#Creates the blank layout file
+$START_MENU_LAYOUT | Out-File $layoutFile -Encoding ASCII
+
+$regAliases = @("HKLM", "HKCU")
+
+#Assign the start layout and force it to apply with "LockedStartLayout" at both the machine and user level
+foreach ($regAlias in $regAliases){
+    $basePath = $regAlias + ":\SOFTWARE\Policies\Microsoft\Windows"
+    $keyPath = $basePath + "\Explorer" 
+    IF(!(Test-Path -Path $keyPath)) { 
+        New-Item -Path $basePath -Name "Explorer"
+    }
+    Set-ItemProperty -Path $keyPath -Name "LockedStartLayout" -Value 1
+    Set-ItemProperty -Path $keyPath -Name "StartLayoutFile" -Value $layoutFile
+}
+
+#Restart Explorer, open the start menu (necessary to load the new layout), and give it a few seconds to process
+Stop-Process -name explorer
+Start-Sleep -s 5
+$wshell = New-Object -ComObject wscript.shell; $wshell.SendKeys('^{ESCAPE}')
+Start-Sleep -s 5
+
+#Enable the ability to pin items again by disabling "LockedStartLayout"
+foreach ($regAlias in $regAliases){
+    $basePath = $regAlias + ":\SOFTWARE\Policies\Microsoft\Windows"
+    $keyPath = $basePath + "\Explorer" 
+    Set-ItemProperty -Path $keyPath -Name "LockedStartLayout" -Value 0
+}
+
+#Restart Explorer and delete the layout file
+Stop-Process -name explorer
+
+
+#endregion
 
 
 
@@ -100,7 +156,7 @@ $regPathSearch           = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Sear
 
 
 
-<#
+<# Optional removal
 
 #region remove bloat
 
